d0c2526d9bb470dac96fcd6c852bbecf8303f328
John Tsevdos <tsevdosjohn@gmail.com>
2021-03-03 00:31:25 +0200

    fix redirects in next.js (removed them from netlify)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+import redirects from "./redirects";
+
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
 
 module.exports = withMDX({
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+  async redirects() {
+    return redirects;
+  },
 });
---
+++ redirects.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    source: "/2007/10/13/new-tsevdos-com/",
+    destination: "/new-tsevdos-com",
+    permanent: true,
+  },
+  {
+    source: "/2007/10/13/previous-weeks-news/",
+    destination: "/new-tsevdos-com",
+    permanent: true,
+  },
+]
