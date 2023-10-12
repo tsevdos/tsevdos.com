4035967f8eb210a2ee80bc8b620f06a6a04b4e95
John Tsevdos <tsevdosjohn@gmail.com>
2023-10-12 14:58:57 +0300

    feat: add rss feed and sitemap (#3)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import lottie from "astro-integration-lottie";
 import mdx from "@astrojs/mdx";
-// import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tsevdos.com",
   compressHTML: true,
-  integrations: [lottie(), tailwind(), mdx() /*sitemap()*/],
+  integrations: [lottie(), tailwind(), mdx(), sitemap()],
 });
