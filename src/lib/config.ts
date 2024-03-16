43ae2d13ca6b950876a4416d91f9f2e3cb042329
John Tsevdos <tsevdosjohn@gmail.com>
2024-03-16 20:12:50 +0200

    chore: add open graph metadata

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,6 @@
 export default {
   title: "Tsevdos.com",
   tagline: "Web design, internet news and blogging tips",
-  url: "https://tsevdos.com",
-  author: "John Tsevdos",
 } as const;
 
 export const POSTS_PER_PAGE = 10;
