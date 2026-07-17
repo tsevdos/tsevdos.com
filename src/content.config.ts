4f05a15f4f6af9801f3240d1a395d5db1283f793
John Tsevdos <tsevdosjohn@gmail.com>
2026-07-17 15:23:36 +0300

    feat: downgrade TS and fix types

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,9 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
 const postsCollection = defineCollection({
-  loader: glob({ base: './src/content/posts', pattern: '**/*.md' }),
+  loader: glob({ base: "./src/content/posts", pattern: "**/*.md" }),
   schema: z.object({
     title: z.string(),
     date: z.string(),
@@ -15,7 +15,7 @@ const postsCollection = defineCollection({
 });
 
 const pagesCollection = defineCollection({
-  loader: glob({ base: './src/content/pages', pattern: '**/*.{md,mdx}' }),
+  loader: glob({ base: "./src/content/pages", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
     title: z.string(),
     date: z.string(),
